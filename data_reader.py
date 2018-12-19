fc315f46199e3a8218b2541c1cbfe5e04ea138a7
Naza <nafm@live.com.ar>
2018-12-19 09:48:56 -0300

    data reader plots modified

--- data_reader.py
+++ data_reader.py
@@ -2,26 +2,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 df = pd.read_csv('champions.csv')
+subplots_pos = [131, 132, 133]
 
-plt.subplot(131)
-df_fsm = df['First Skill Maxed'].value_counts()
-plt.pie(df_fsm, labels=df_fsm.index, startangle=90, autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('First Skill Maxed')
+plt.figure(0)
+for idx, skill in enumerate(list(df.keys()[1:4])):
+        plt.subplot(subplots_pos[idx])
+        df_fsm = df[skill].value_counts()
+        plt.pie(df_fsm, labels=df_fsm.index, startangle=90, autopct='%1.1f%%')
+        plt.axis('equal')
+        plt.title(skill)
 
-plt.subplot(132)
-df_ssm = df['Second Skill Maxed'].value_counts()
-plt.pie(df_ssm, labels=df_ssm.index, startangle=90, autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('Second Skill Maxed')
 
-plt.subplot(133)
-df_tsm = df['Third Skill Maxed'].value_counts()
-plt.pie(df_tsm, labels=df_tsm.index, startangle=90, autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('Third Skill Maxed')
 
-plt.show()
+plt.figure(1)
 plt.pie(df['Skill Priority'].value_counts(), labels= df['Skill Priority'].value_counts().index, startangle=90,
         autopct='%1.1f%%')
+plt.axis('equal')
 plt.show()
