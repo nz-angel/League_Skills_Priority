3d35a232dfc7a51196d40f248ea9ac4ff4afdc9d
Naza <nafm@live.com.ar>
2019-01-26 19:08:50 -0300

    Data updated (patch 9.2)

--- data_collector.py
+++ data_collector.py
@@ -4,6 +4,7 @@ import csv
 import requests
 import json
 from lxml import html
+from tqdm import tqdm
 
 
 def get_skill_order(t):
@@ -34,13 +35,61 @@ def main():
     champion_data = json.loads(requests.get(ddragon_url).content)
     champions = [x for x in champion_data['data'].keys()]
 
+    # Get class, subclass and release date of every champion (pulled from League of Legends wiki)
+    champion_rd = {champ: 2019 for champ in champions}
+    champion_class = {champ: 0 for champ in champions}
+    champion_subclass = {champ: 0 for champ in champions}
+    subclass_belonging = {'Enchanter': 'Controller',
+                          'Catcher': 'Controller',
+                          'Diver': 'Fighter',
+                          'Juggernaut': 'Fighter',
+                          'Burst': 'Mage',
+                          'Battlemage': 'Mage',
+                          'Artillery': 'Mage',
+                          'Marksman': 'Marksman',
+                          'Assassin': 'Slayer',
+                          'Skirmisher': 'Slayer',
+                          'Vanguard': 'Tank',
+                          'Warden': 'Tank',
+                          'Specialist': 'Specialist'}
+    wiki_class_page = requests.get('https://leagueoflegends.fandom.com/wiki/List_of_champions')
+    tree = html.fromstring(wiki_class_page.content)
+    for idx, champ in enumerate(champions):
+        subclass = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[{}]/td[2]/@data-sort-value'.format(idx+2))[0]
+        release_date = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[{}]/td[4]'.format(idx+2))[0].text
+        if release_date == ' ':
+            release_date = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[{}]/td[4]/span'.format(idx + 2))[0].text
+
+        champion_subclass[champ] = subclass
+        champion_class[champ] = subclass_belonging[subclass]
+        champion_rd[champ] = release_date.strip()[:4]
+
+    # Get dates for champions who received a complete VGU (pulled from League of Legends wiki) who are described as
+    # getting a 'Full relauch'
+    champion_full_vgu = {champ: champion_rd[champ] for champ in champions}
+    wiki_update_page = requests.get('https://leagueoflegends.fandom.com/wiki/Champion_updates')
+    tree = html.fromstring(wiki_update_page.content)
+    i = 2
+    while True:
+        try:
+            update_desc = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[{}]/td[2]/a'.format(i))[0].text.strip()
+        except IndexError:
+            break
+
+        if 'Full Relaunch' in update_desc:
+            champ = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[{}]/td[1]/@data-sort-value'.format(i))[0]
+            date = tree.xpath('//*[@id="mw-content-text"]/table[2]/tr[{}]/td[3]'.format(i))[0].text
+            if date.strip().startswith('20'):
+                champion_full_vgu[champ] = date.strip()[:4]
+        i += 1
+    champion_full_vgu['Aatrox'] = 2018  # Aatrox received a full VGU in 2018 but the Wiki only has 'VGU'
+
     # Skill leveling priority is pulled from champion.gg
     skill_priority = {champ: [] for champ in champions}
     first_lvl_skill = {champ: [] for champ in champions}
-    champion_class = {champ: champion_data['data'][champ]['tags'][0] for champ in champions}
     wait_time = [1.5, 2]
 
-    for c in champions:
+    for c in tqdm(champions):
         page = requests.get('https://champion.gg/champion/'+c)
         tree = html.fromstring(page.content)
         skill_priority[c], first_lvl_skill[c] = get_skill_order(tree)
@@ -50,10 +99,11 @@ def main():
     with open('champions.csv', 'w') as f:
         data_writer = csv.writer(f, dialect='excel')
         data_writer.writerow(['Champion', 'First Skill Maxed', 'Second Skill Maxed', 'Third Skill Maxed',
-                              'First Skill', 'Skill Priority', 'Main Class'])
+                              'First Skill', 'Skill Priority', 'Class', 'Subclass', 'Release Date', 'Release Date (VGU)'])
         for c in champions:
             data_writer.writerow([c, skill_priority[c][0], skill_priority[c][1], skill_priority[c][2],
-                                  first_lvl_skill[c], ''.join(skill_priority[c]), champion_class[c]])
+                                  first_lvl_skill[c], ''.join(skill_priority[c]), champion_class[c],
+                                  champion_subclass[c], champion_rd[c], champion_full_vgu[c]])
 
 
 if __name__ == "__main__":
